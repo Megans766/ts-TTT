dafd9b1d905d4dde9fd23dfc67df90de01f05908
Megan Smith <megans766@gmail.com>
2023-02-20 12:34:05 -0500

    create state variables, constants, and cached elements

---
+++ src/app.ts
@@ -0,0 +1,37 @@
+// Constants
+const winCombos = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 4, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [2, 4, 6]
+]
+
+//Variables (state)
+let board: number[], turn: number, winner: boolean, tie: boolean
+
+//Cached Element References
+const squareEls = document.querySelectorAll('.sqr')
+const messgaeEl = document.getElementById('message')
+const resetBtn = document.querySelector<HTMLButtonElement>('#reset')
+
+//Event Listeners
+// squareEls.addEventlistener('click', handleClick)
+resetBtn?.addEventListener('click', init)
+
+//Functions
+function init() {
+  board = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  turn = 1
+  winner = false
+  tie = false
+}
+
+init ()
+
+function handleClick(evt: MouseEvent): void {
+
+}
